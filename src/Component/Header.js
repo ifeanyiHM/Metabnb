268dfb0321bf35f78c1471546d19165123f53448
ifeanyihm <iheme.ifeanyi@yahoo.com>
2022-11-13 14:56:34 +0100

    testing and updating

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -11,23 +11,23 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
 
-    // const [open, setOpen] = useState("trans");
-    // const handleClick = () => {
-    //     if (open == "trans") {
-    //         setOpen("trans trans_back");
-    //     }
-    // }
+    const [open, setOpen] = useState("trans");
+    const handleClick = () => {
+        if (open == "trans") {
+            setOpen("trans trans_back");
+        }
+    }
 
-    // document.querySelector(".exit").addEventListener('click', function() {
-    //     setOpen("trans");
-    // })
+    document.querySelector(".exit").addEventListener('click', function() {
+        setOpen("trans");
+    })
 
-    const [modal, setModal] = useState("false");
+    const [modal, setModal] = useState("true");
     const toggleModal = () => {
         setModal(!modal);
     }
 
-    if (modal) {
+    if (!modal) {
         document.body.classList.add('active');
     } else{
         document.body.classList.remove('active');
@@ -37,7 +37,7 @@ const Header = () => {
         <div className="Header">
            <div className="Navigation">
                 <img className="logo" src={logo} alt="logo" />
-                <div className="trans"/*{open}*/>
+                <div className={open}>
                     <img className="exit" src={exit} alt="exit" />
                         
                     <ul>
@@ -52,10 +52,10 @@ const Header = () => {
                     <p>Connect wallet</p>
                 </div>
                
-                <img className="media_nav" /*onClick={handleClick}*/ src={nav} alt="nav_menu" />
+                <img className="media_nav" onClick={handleClick} src={nav} alt="nav_menu" />
            </div>
 
-            {modal && (
+            {!modal && (
                 <div className="modal">
                 <div className="fixed_modal"  onClick={toggleModal}></div>
                     <div className="content">
